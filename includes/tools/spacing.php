1cafad456ddb4b696e438478889cb5d5127ec5ba
Pablo Impallari <impallari@gmail.com>
2016-05-05 18:07:54 -0300

    Tools - Spacing Grid: Adding text shorcuts

--- includes/tools/spacing.php
+++ includes/tools/spacing.php
@@ -14,11 +14,21 @@
 			<tr>
 				<td>
 					<p class="sizelabel">Glyphs:</p>
-					<input type="text" name="nnxnnglyphs" id="nnxnnglyphs" size="50" value="<?php if (isset($_POST['nnxnnglyphs']) && !empty($_POST['nnxnnglyphs']) ) echo $_POST['nnxnnglyphs']; ?>" />
+					<input type="text" name="nnxnnglyphs" id="nnxnnglyphs" size="70" value="<?php if (isset($_POST['nnxnnglyphs']) && !empty($_POST['nnxnnglyphs']) ) echo $_POST['nnxnnglyphs']; ?>" />
+					<p class="sizelabel" style="margin-top: 10px;">
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('adhesion'); return false;">adhesion</a> - 
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('abcdefghijklmnopqrstuvwxyz'); return false;">a-z</a> - 
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('ABCDEFGHIJKLMNOPQRSTUVWXYZ'); return false;">A-Z</a> -  
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('0123456789'); return false;">0-9</a> -  
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('à á â ã ä å ā ă ą ǻ ȁ ȃ æ ǽ ć ç ĉ ċ č ď đ ð ḍ ǳ ǆ è é ê ë ē ĕ ė ę ě ẽ ẹ ȅ ȇ ə ǵ ĝ ğ ġ ģ ĥ ħ ḥ ì í î ï ĩ ī į ĭ ị ȉ ȋ ĳ ĵ ķ ĸ ĺ ļ ľ ŀ ł ǉ ń ŉ ņ ň ñ ṅ ŋ ǌ ò ó ô õ ö ø ō ŏ ő ǿ ǫ ọ ȍ ȏ œ þ ŕ ŗ ř ṛ ȑ ȓ ś ŝ š ș ş ṣ ß ŧ ť ț ţ ṭ ù ú û ü ũ ū ŭ ů ű ų ụ ȕ ȗ µ μ ẁ ẃ ẅ ŵ ý ÿ ŷ ỳ ỹ ź ż ž ẓ'); return false;">latin</a> - 
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('À Á Â Ã Ä Å Ā Ă Ą Ǻ Ȁ Ȃ Æ Ǽ Ć Ç Ĉ Ċ Č Ď Đ Ð Ḍ Ǳ ǲ Ǆ ǅ È É Ê Ë Ē Ĕ Ė Ę Ẽ Ě Ẹ Ȅ Ȇ Ə Ǵ Ĝ Ğ Ġ Ģ Ĥ Ħ Ḥ Ĭ Ì Í Î Ï İ Ĩ Ī Į Ị Ȉ Ȋ Ĳ Ĵ Ķ Ĺ Ļ Ľ Ŀ Ł Ǉ ǈ Ñ Ń Ņ Ň Ṅ Ŋ Ǌ ǋ Ò Ó Ô Õ Ö Ō Ŏ Ő Ø Ǿ Ǫ Ọ Ȍ Ȏ Œ Þ Ŕ Ŗ Ř Ṛ Ȑ Ȓ Ś Ŝ Š Ș Ş Ṣ Ŧ Ť Ț Ţ Ṭ Ù Ú Û Ü Ũ Ū Ŭ Ů Ű Ų Ụ Ȕ Ȗ Ẁ Ẃ Ẅ Ŵ Ý Ŷ Ÿ Ỳ Ỹ Ź Ż Ž Ẓ'); return false;">LATIN</a> -
+						<a nohref style="cursor:pointer;color:blue;text-decoration:none" onclick="$('#nnxnnglyphs').val('# % ° _ = ± + ÷ − - < > × . , : ; | ½ ⅓ ⅔ ¼ ¾ ⅛ ⅜ ⅝ ⅞ ¢ $ € £ '); return false;">Math</a></p>
+					<p class="sizelabel" style="margin-top: 6px;">Punctation: ‐ ― ~ _ . … , ' " „ ‘ ’ ‛ “ ” ‟ : ; ‹ « › » \ /  • * † ‡ &ampp; @ ¿ ? ¡ ! ( ) [ ] { } ª º © ® ¶ # §</p>	
 				</td>
 				<td>
 					<p class="sizelabel">Size:</p>
-					<input type="text" name="nnxnntamanio" id="nnxnntamanio" size="3" value="<?php if (isset($_POST['nnxnntamanio']) && !empty($_POST['nnxnntamanio']) ) echo $_POST['nnxnntamanio']; ?>" />				
+					<input type="text" name="nnxnntamanio" id="nnxnntamanio" size="3" value="<?php if (isset($_POST['nnxnntamanio']) && !empty($_POST['nnxnntamanio']) ) echo $_POST['nnxnntamanio']; ?>" />
+					<p class="sizelabel" style="margin-top: 10px;">&nbsp;</p>				
 				</td>
 				<td>
 					<p class="sizelabel">
