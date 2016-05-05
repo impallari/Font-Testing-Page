7805ae5056221fa2c359200f4c8203e2398b0be6
Pablo Impallari <impallari@gmail.com>
2016-05-05 20:38:35 -0300

    Made font sizer independent of each other

--- includes/latin-02/capstolower-short.php
+++ includes/latin-02/capstolower-short.php
@@ -1,21 +1,21 @@
 <div contenteditable="true" style="width: 920px;">
 
 	<div class="dontprint">
-		<input type="range" min="12" max="200" value="40" step="1" oninput="changeFontSize(this.value)" onchange="changeFontSize(this.value)" style="width: 300px;" />
-		<input type="range" min="0" max="9" value="5" step="1" oninput="changeLineHeigth(this.value)" onchange="changeLineHeigth(this.value)" style="width: 100px;" />
+		<input type="range" min="12" max="200" value="40" step="1" oninput="changeFontSizeC2L(this.value)" onchange="changeFontSizeC2L(this.value)" style="width: 300px;" />
+		<input type="range" min="0" max="9" value="5" step="1" oninput="changeLineHeigthC2L(this.value)" onchange="changeLineHeigthC2L(this.value)" style="width: 100px;" />
 		<script type="text/javascript">
-			function changeFontSize(newValue) {
-			document.getElementById("changedfontsize").innerHTML=newValue;
-				$('.changesize').css('font-size',newValue + 'px');
+			function changeFontSizeC2L(newValue) {
+			document.getElementById("changedfontsizeC2L").innerHTML=newValue;
+				$('.changesizeC2L').css('font-size',newValue + 'px');
 			};
-			function changeLineHeigth(newValue) {
-			document.getElementById("changedlineheigth").innerHTML=newValue;
-				$('.changesize').css('line-height','1.'+newValue);
+			function changeLineHeigthC2L(newValue) {
+			document.getElementById("changedlineheigthC2L").innerHTML=newValue;
+				$('.changesizeC2L').css('line-height','1.'+newValue);
 			};
 		</script>
 	</div>
 
-	<p class="sizelabel"><span id="changedfontsize">40</span>px / 1.<span id="changedlineheigth">5</span>em</p>
-	<p class="changesize" style="font-size: 40px; line-height: 1.5em;">Azoturia Ambidextrous Almarjo Blowing Bylane Basketball China Convened Chronology Duckbilled Desuncir Dowsing Elites Enamorar Exiguousness Flavor Fittipaldi Feminine Glasslike Gigantesco Golfear Holding Haymaker Hijos Indiferente Ildefonso Illogical Jellyrolls Jeopardize Jalbegar Klaxon Knickers Keyed Llave Loans Lightboard Levying Myths Mud Mieux Marxi Motor Nitpicked Necrophobe Notarizes Olmos Operosely Obsequie Plighted Presby Pedalfer Pipiolo Quilmes Qingdao Qayrawan Rafted Ryder Riquelme Savors Sacrificers Selfinterest Thirtytwo Tarija Talons Ultimate Unquietness Vladimir Viaje Viticultura Weirder Whizzes Wielding Xylophon Xilita Xifoides Yielded Yoga Yttrium Zipper Zombie Zillions</p>
+	<p class="sizelabel"><span id="changedfontsizeC2L">40</span>px / 1.<span id="changedlineheigthC2L">5</span>em</p>
+	<p class="changesizeC2L" style="font-size: 40px; line-height: 1.5em;">Azoturia Ambidextrous Almarjo Blowing Bylane Basketball China Convened Chronology Duckbilled Desuncir Dowsing Elites Enamorar Exiguousness Flavor Fittipaldi Feminine Glasslike Gigantesco Golfear Holding Haymaker Hijos Indiferente Ildefonso Illogical Jellyrolls Jeopardize Jalbegar Klaxon Knickers Keyed Llave Loans Lightboard Levying Myths Mud Mieux Marxi Motor Nitpicked Necrophobe Notarizes Olmos Operosely Obsequie Plighted Presby Pedalfer Pipiolo Quilmes Qingdao Qayrawan Rafted Ryder Riquelme Savors Sacrificers Selfinterest Thirtytwo Tarija Talons Ultimate Unquietness Vladimir Viaje Viticultura Weirder Whizzes Wielding Xylophon Xilita Xifoides Yielded Yoga Yttrium Zipper Zombie Zillions</p>
 
 </div>	
