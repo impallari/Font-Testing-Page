c366b0802b8a74425b9c07ca2b411a7b6aeb20b5
Pablo Impallari <impallari@gmail.com>
2016-05-05 20:16:38 -0300

    Some more content for Latin2

--- includes/latin-02/pangra.php
+++ includes/latin-02/pangra.php
@@ -3,20 +3,27 @@
 	<p class="sizelabel">46px</p>
 	<p style="font-size: 46px; line-height: 1.5em;">Azulejo Boxeo Clavija Duquesa Estupendo Fetichismo Graficos Hockey Iceberg Jockey Kilowatt Lavadero Memorizar Nixon Ochenta Preferible Quiroga Rawson Sequoia Tango Usurpar Verificar Whisky Xenofobia Yacimiento Zapador</p>
 	<p>&nbsp;</p>
+
+	<p style="font-size: 46px; line-height: 1.5em;">Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey Xray Yankee Zulu</p>
+	<p>&nbsp;</p>
+
+	<p style="font-size: 46px; line-height: 1.5em;">Aaron Briana Colby Dakota Earlene Felice Gavin Holly IndigoJerrell Karena Logan Miranda Nelson Odessa Perry Quinn Rachel Simone Taylor Ursula Vida Willem Xiomara Yvonne Zachary</p>
+	<p>&nbsp;</p>
+
+	<p style="font-size: 46px; line-height: 1.5em;">Alissa Bernhard Caprice Derrick Erica Franklin Guinevere Harold Imogene Jason Kamelia Lawrence Miranda Nicholas Olivia Phillip Querida Rashad Sumiko Thomas Ursula Vincent Willow Xiomar Yvonne Zachary</p>
+	<p>&nbsp;</p>
 	
 	<p class="sizelabel">28px</p>
 	<p style="font-size: 28px;">few black taxis drive up major roads on quiet hazy nights</p>
 	<p>&nbsp;</p>
 	
-	<p class="sizelabel">28px</p>
 	<p style="font-size: 28px;">el pinguino wenceslao hizo kilometros bajo exhaustiva lluvia y frio anoraba su querido cachorro</p>
 	<p>&nbsp;</p>
 	
-	<p class="sizelabel">28px</p>
 	<p style="font-size: 28px;">jovencillo emponzonado de whisky que figurota exhibe</p>
 	<p>&nbsp;</p>
 		
-	<p class="sizelabel">28px</p>
 	<p style="font-size: 28px;">queda gazpacho fibra latex jamon kiwi y vinos</p>
+	<p>&nbsp;</p>
 
 </div>	
