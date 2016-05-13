44c17641995716b50e54cf53ebe2b079d5fe9a39
Pablo Impallari <impallari@gmail.com>
2016-05-13 00:34:43 -0300

    More content in the Lettering Tab
    Also new arrangement for the caps and lowercases

--- includes/latin/lettering.php
+++ includes/latin/lettering.php
@@ -1,12 +1,18 @@
-<div contenteditable="true" style="width: 880px; text-align: center; padding: 20px 20px;">
+<div contenteditable="true" style="width: 920px; text-align: center; padding: 20px 20px;">
 	
-	<p class="sizelabel">Lettering Sheet 72 points</p>
+	<p class="sizelabel" style="margin-bottom: 30px;">Lettering Sheet 72 points</p>
 	
-	<p style="font-size: 72px;">A B C D E F G H I J K L M N O P Q R S T U V W X Y Z<br />
-	a b c d e f g h i j k l m n o p q r s t u v w x y z<br />
+	<p style="font-size: 72px;">A B C D E F G H<br />
+	I J K L M N O P Q R<br />
+	S T U V W X Y Z<br />
+	a b c d e f g h i j k l m<br />
+	n o p q r s t u v w x y z<br />
+	<br />	
 	1 2 3 4 5 6 7 8 9 0<br />
 	<br />
-	( { [ . , ¡ ! ¿ ? * ' ‘ ’ " “ ” ] } )<br />
-	$ € £ % @ &amp; ¶ § ¢ † ‡ </p>
+	( { [ . , ¡ ! ¿ ? * ] } )<br />
+	# $ € £ % @ &amp; ¶ § ¢ † ‡<br />
+	° _ : = ; + − - × ÷ ' ‘ ’ " “ ”<br />
+	&#60; &#62; ≤ ± ≥ « ‹ © ® › »</p>
 	
 </div>	
