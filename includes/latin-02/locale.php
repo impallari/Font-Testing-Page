6963e755ee39d3ebecd92b0bf93518f3581c435e
Simon Cozens <simon@simon-cozens.org>
2017-07-01 13:38:02 +1000

    Some more OpenType localisation tests

--- includes/latin-02/locale.php
+++ includes/latin-02/locale.php
@@ -4,7 +4,7 @@
 	<p>&nbsp;</p>
 	
 	<table cellpadding="14">
-		<tr><td colspan="3"><p class="sizelabel" style="text-align: center ;">Catalan</p></td></tr>
+		<tr><td colspan="3"><p class="sizelabel" style="text-align: center ;">Catalan punt volat</p></td></tr>
 		<tr>
 			<td><p class="sizelabel" style="text-align: right ;">Using /l/periodcentered and no lang tag</p></td>
 			<td><p style="font-size: 46px; line-height: 1.5em;">Il·lustració cel·la</p></td>			
@@ -21,6 +21,63 @@
 			<td><p style="font-size: 46px; line-height: 1.5em;">Iŀlustració ceŀla</p></td>
 			<td><p style="font-size: 46px; line-height: 1.5em;">COĿLEGI CEĿLA</p></td>
 		</tr>
+
+		<tr><td colspan="3"><p class="sizelabel" style="text-align: center ;">Polish kreska</p></td></tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">No lang tag</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;">źródłosłowu</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;">ŹRÓDŁOSŁOWU</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Portuguese (o-acute and lang="pt")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="pt">espanhóis</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="pt">ESPANHÓIS</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Polish (o-acute and lang="pl")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="pl">źródłosłowu</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="pl">ŹRÓDŁOSŁOWU</p></td>
+		</tr>
+
+		<tr><td colspan="3"><p class="sizelabel" style="text-align: center ;">Native American Ogonek</p></td></tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">No lang tag</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;">ałʼąą</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;">AŁʼĄĄ</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Polish (a-ogonek and lang="pl")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="pl">robią</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="pl">ROBIĄ</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Navajo (a-ogonek and lang="nv")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="nv">ałʼąą</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="nv">AŁʼĄĄ</p></td>
+		</tr>
+
+		<tr><td colspan="3"><p class="sizelabel" style="text-align: center ;">Cyrillic Variants</p></td></tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">No lang tag</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;">добра</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;">ДОБРА</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Russian (lang="ru")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="ru">добра</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="ru">ДОБРА</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Serbian (lang="sr")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="sr">добра</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="sr">ДОБРА</p></td>
+		</tr>
+		<tr>
+			<td><p class="sizelabel" style="text-align: right ;">Bulgarian (lang="bg")</p></td>
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="bg">добра</p></td>			
+			<td><p style="font-size: 46px; line-height: 1.5em;" lang="bg">ДОБРА</p></td>
+		</tr>
+
 	</table>
 		
 	<p>&nbsp;</p>
