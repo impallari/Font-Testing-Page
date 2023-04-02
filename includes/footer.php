de49ac5cf90628be315ac3fdb0f0b11d60ea885c
Pablo Impallari <impallari@gmail.com>
2023-04-02 04:07:40 -0300

    Added a footer link to Peter's Fontproffer
    And also increased the footer font size from 10 to 16px. I'm getting fucking blind, can't read 10px fonts anymore :) Fuck it!

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
 <section id="footer">
-	<p>The Font Testing Page is a collective project made possible by a long list of wonderful people contributing together. Join us at <a href="https://github.com/impallari/font-testing-page">GitHub</a></p>
+	<p>The Font Testing Page is made possible by a long list of wonderful people contributing together. <a href="https://github.com/impallari/font-testing-page">GitHub</a></p>
+	<p>If you are using The Testing Page, you will also love the awesome <a href="https://fontproofer.com/">FontProofer App by Peter Nowel</a>. Check it out!</p>
 </section>
