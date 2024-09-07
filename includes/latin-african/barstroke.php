e88993ce28f2bb15d824c1f86d272ed8fe02f120
Pablo Impallari <impallari@gmail.com>
2024-09-06 22:52:12 -0300

    Adding Lslash and Oslahs to "Strokes & Bars" tab

--- includes/latin-african/barstroke.php
+++ includes/latin-african/barstroke.php
@@ -1,10 +1,10 @@
 <div contenteditable="true" style="width: 980px; text-align: center; padding: 20px 20px;">
 	
-	<p class="sizelabel" style="margin-bottom: 30px;">Bar & Stroke - 62 points</p>
+	<p class="sizelabel" style="margin-bottom: 30px;">Bars,Strokes and Slashes - Check for consistency - 62 points</p>
 	
 	<p style="font-size: 62px;">Ⱥ Ƀ Ꞓ Ȼ Ɖ Ð Ɇ Ǥ Ħ Ɨ Ɉ<br />
-	Ꝁ Ƚ Ⱡ Ᵽ Ɍ Ŧ Ⱦ Ʉ Ɏ Ƶ<br />
+	Ꝁ Ƚ Ⱡ Ł Ø Ᵽ Ɍ Ŧ Ⱦ Ʉ Ɏ Ƶ<br />
 	ⱥ ƀ ꞓ ȼ ɇ ǥ ħ ɨ ɉ<br />
-	ꝁ ⱡ ᵽ ɍ ŧ ⱦ ʉ ɏ ƶ</p>
+	ꝁ ⱡ ł ø ᵽ ɍ ŧ ⱦ ʉ ɏ ƶ</p>
 	
 </div>	
