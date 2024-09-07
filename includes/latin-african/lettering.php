45f7006a8706433d0ba67da8b0cbef1b014c0c6d
Pablo Impallari <impallari@gmail.com>
2024-09-07 09:35:06 -0300

    Adding Eng & eng to African Lettering Tab

--- includes/latin-african/lettering.php
+++ includes/latin-african/lettering.php
@@ -3,9 +3,9 @@
 	<p class="sizelabel" style="margin-bottom: 30px;">African Lettering Sheet - 62 points</p>
 	
 	<p style="font-size: 62px;">Ɑ Ɓ Ƈ Ɗ Ɖ Ɛ Ǝ Ʃ Ʒ Ƒ Ɠ Ɦ Ɥ Ɩ<br />
-	Ƙ Ɲ Ɔ Ƥ Ɽ Ƭ Ʈ Ʊ Ɣ Ʋ Ʌ Ⱳ Ƴ<br />
+	Ƙ Ɲ Ŋ Ɔ Ƥ Ɽ Ƭ Ʈ Ʊ Ɣ Ʋ Ʌ Ⱳ Ƴ<br />
 	<br />	
-	ɑ ɓ ƈ ɗ ɖ ɛ ʃ ǝ ʒ ɣ ɠ ɂ ɦ ɩ ƙ ɲ<br />
+	ɑ ɓ ƈ ɗ ɖ ɛ ʃ ǝ ʒ ɣ ɠ ɂ ɦ ɩ ƙ ɲ ŋ<br />
 	ƥ ɔ ɋ ɾ ɽ ƭ ʈ ʊ ʋ ʌ ⱳ ƴ ʔ ʕ Ꞌ ꞌ</p>
 	
 </div>	
