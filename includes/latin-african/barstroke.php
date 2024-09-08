3309b50cd632ef93870e7d3a0437c790d14c9042
Pablo Impallari <impallari@gmail.com>
2024-09-08 03:14:10 -0300

    Improving Tab description

--- includes/latin-african/barstroke.php
+++ includes/latin-african/barstroke.php
@@ -1,6 +1,6 @@
 <div contenteditable="true" style="width: 980px; text-align: center; padding: 20px 20px;">
 	
-	<p class="sizelabel" style="margin-bottom: 30px;">Bars,Strokes and Slashes - Check for consistency - 62 points</p>
+	<p class="sizelabel" style="margin-bottom: 30px;">Bar, Stroke and Slash realted African and Latin Glpyhs: Check for consistency - 62 points</p>
 	
 	<p style="font-size: 62px;">Ⱥ Ƀ Ꞓ Ȼ Ɖ Ð Ɇ Ǥ Ħ Ɨ Ɉ<br />
 	Ꝁ Ƚ Ⱡ Ł Ø Ᵽ Ɍ Ŧ Ⱦ Ʉ Ɏ Ƶ<br />
